7e9f8042313c17ab80404342ccb417c0e61b76cd
RITIK-SHARMA97 <sharmaritik1297@gmail.com>
2025-11-09 16:03:37 +0530

    added entity and repository

---
+++ src/test/java/com/ritik/HOSPITAL/MANAGEMENT/HospitalManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ritik.HOSPITAL.MANAGEMENT;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HospitalManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
